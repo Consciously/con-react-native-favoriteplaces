38f5209d767b0763b1a59f51c7687249d1c90f46
Stefan Ihle <stefanihle@gmx.net>
2022-06-10 09:50:56 +0200

    adding interactive map

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -4,12 +4,15 @@ import {
 	useForegroundPermissions,
 	PermissionStatus
 } from 'expo-location';
+import { useNavigation } from '@react-navigation/native';
 import OutlinedButton from '../ui/OutlinedButton';
 import { Colors } from '../../constants/colors';
 import { useState } from 'react';
 import { getMapPreview } from '../../util/location';
 
 const LocationPicker = () => {
+	const navigation = useNavigation();
+
 	const [pickedLocation, setPickedLocation] = useState();
 
 	const [locationPermissionInformation, requestPermission] =
@@ -50,7 +53,9 @@ const LocationPicker = () => {
 		});
 	};
 
-	const pickOnMapHandler = () => {};
+	const pickOnMapHandler = () => {
+		navigation.navigate('Map');
+	};
 
 	let locationPreview = <Text>No location picked yet.</Text>;
 
