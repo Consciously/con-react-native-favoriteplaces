66ce6fc4202277840bb1618acbca232b8d42e47c
Stefan Ihle <stefanihle@gmx.net>
2022-06-13 15:59:23 +0200

    replace expo-app-loading package with expo-splash-screen packge due deprecation warning

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AppLoading from 'expo-app-loading';
+// import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import Map from './screens/Map';
@@ -14,24 +15,33 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
 	const [dbInitialized, setDbInitialized] = useState(false);
+
 	useEffect(() => {
-		init()
-			.then(() => {
+		const prepare = async () => {
+			try {
+				await SplashScreen.preventAutoHideAsync();
+				init();
+			} catch (e) {
+				console.warn(e);
+			} finally {
 				setDbInitialized(true);
-			})
-			.catch(err => {
-				console.log(err);
-			});
+			}
+		};
+		prepare();
 	}, []);
 
-	if (!dbInitialized) {
-		return <AppLoading />;
-	}
+	const onLayoutRootView = useCallback(async () => {
+		if (dbInitialized) {
+			await SplashScreen.hideAsync();
+		}
+	}, [dbInitialized]);
+
+	if (!dbInitialized) return null;
 
 	return (
 		<>
 			<StatusBar style='dark' />
-			<NavigationContainer>
+			<NavigationContainer onReady={onLayoutRootView}>
 				<Stack.Navigator
 					screenOptions={{
 						headerStyle: {
